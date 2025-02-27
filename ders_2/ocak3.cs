fc016c40879dfad1d3fd596bb5115ade44eb3fdc
MehmetUlDG <mehmetuludag2021@outlook.com>
2025-02-27 11:21:40 +0300

    hatalar düzeltildi.

--- ders_2/ocak3.cs
+++ ders_2/ocak3.cs
@@ -3,21 +3,23 @@
     {
         static void Main(string[] args)
         {
-             System.Console.WriteLine("bir sayı giriniz.");
-             int sayi = Convert.ToInt32(Console.ReadLine());
-             int toplam=0;
-             int toplam2=0;
-             int döngü=1;
-             while(toplam!=sayi)
-             {
-                 toplam=toplam+döngü;
-                    döngü++;
-             }
-                System.Console.WriteLine("girilen sayıya kadar olan sayıların toplamı(while döngüsü): "+toplam);
+                System.Console.WriteLine("bir sayı giriniz.");
+    int sayi = Convert.ToInt32(Console.ReadLine());
+    int toplam = 0;
+    int toplam2 = 0;
+    int döngü=1;
+    while (döngü!= sayi+1)
+    {
+        toplam = toplam + döngü;
+        döngü++;
+    }
+    System.Console.WriteLine("girilen sayıya kadar olan sayıların toplamı(while döngüsü): " + toplam);
 
-                for(int i=1;i<=sayi;i++)
-                {
-                    toplam2=toplam2+i;
+    for (int i = 1; i <= sayi; i++)
+    {
+        toplam2 = toplam2 + i;
 
-                }
-                System.Console.WriteLine("girilen sayıya kadar olan sayıların toplamı(for döngüsü): "+toplam2);}}
+    }
+    System.Console.WriteLine("girilen sayıya kadar olan sayıların toplamı(for döngüsü): " + toplam2);
+}
+    }
--- ders_2/ocak4.cs
+++ ders_2/ocak4.cs
@@ -4,39 +4,41 @@
         static void Main(string[] args)
 
         {  
+    int[] sayilar = new int[5];
+    for (int i = 0; i < 5; i++)
+    {
+        System.Console.WriteLine("bir sayı giriniz.");
+        sayilar[i] = Convert.ToInt32(Console.ReadLine());
+    }
 
-            int[] sayilar = new int[5];
-            for(int i=0;i<5;i++)
+
+    System.Console.WriteLine("en büyük sayı: " + enbuyuk(sayilar));
+    System.Console.WriteLine("en küçük sayı: " + enkucuk(sayilar));
+}
+
+static int enbuyuk(int[] sayilar)
+{
+    int enbuyuk = sayilar[0];
+    for (int i = 0; i < 5; i++)
+    {
+        if (sayilar[i] > enbuyuk)
+        {
+            enbuyuk = sayilar[i];
+        }
+    }
+    return enbuyuk;
+}
+   static int enkucuk(int[] sayilar)
+    {
+        int enkucuk = sayilar[0];
+        for (int i = 0; i < 5; i++)
+        {
+            if (sayilar[i] < enkucuk)
             {
-                System.Console.WriteLine("bir sayı giriniz.");
-                sayilar[i]=Convert.ToInt32(Console.ReadLine());
+                enkucuk = sayilar[i];
             }
-           
-           public static int enbuyuk(int[] sayilar)
-           {
-               int enbuyuk=sayilar[0];
-               for(int i=0;i<5;i++)
-               {
-                   if(sayilar[i]>enbuyuk)
-                   {
-                       enbuyuk=sayilar[i];
-                   }
-               }
-               return enbuyuk;
-           }
+        }
+        return enkucuk;
+    }
 
-           public static int enkucuk(int[] sayilar)
-           {
-               int enkucuk=sayilar[0];
-               for(int i=0;i<5;i++)
-               {
-                   if(sayilar[i]<enkucuk)
-                   {
-                       enkucuk=sayilar[i];
-                   }
-               }
-               return enkucuk;
-           }
-              System.Console.WriteLine("en büyük sayı: "+enbuyuk(sayilar));
-                System.Console.WriteLine("en küçük sayı: "+enkucuk(sayilar));
-        }}
+        }
